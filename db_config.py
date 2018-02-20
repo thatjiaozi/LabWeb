51bdd2cea978f5f6a8d679f046c127d1c22b13b1
Juan Jose Lopez Jaimez <jj.lopezjaimez@gmail.com>
2018-02-20 14:31:19 -0600

    add the code necesswary to setup the development environment of the db

---
+++ db_config.py
@@ -0,0 +1,23 @@
+import os
+import sys
+from urllib.parse import urlparse
+import mysql.connector
+
+# Set the configuration to the connection of the database
+url = urlparse(os.environ['CLEARDB_DATABASE_URL'])
+hostname = url.hostname
+user = url.username
+password = url.password
+port = url.port
+database = url.path[1:]
+
+# Create the connection
+try:
+    database_connection = mysql.connector.connect(host=hostname, user=user, passwd=password, db=database)
+except mysql.connector.Error as err:
+    print("Error while connecting to the database: %d" % err.errno)
+else:
+
+    # Put database code here
+
+    database_connection.close();
