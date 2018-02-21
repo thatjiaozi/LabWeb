7167616fd4a1aca482e476089f48c05e981ef746
Barbara Valdez <babe-hime@hotmail.com>
2018-02-20 22:22:09 -0600

    Frontend de Landing Page
    Frontend de Landing Page
    
    Change-Id: I48c1d2248844c2ae4dbb993863fd200261981bee

--- LabWeb/settings.py
+++ LabWeb/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cloud_system.apps.CloudSystemConfig',
+    'local_system.apps.LocalSystemConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'LabWeb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- cloud_system/views.py
+++ cloud_system/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world! This is the cloud system!!")
+    return render(request,'cloud_system/index.html',{})
