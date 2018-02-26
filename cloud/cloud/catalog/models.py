e6d27ef7703ad05990890fdb66d762adfaecf369
Juan Jose Lopez Jaimez <jj.lopezjaimez@gmail.com>
2018-02-25 23:13:35 -0600

    add data model for the app
    Change-Id: If8571a2675cda4c3ba5428dccac3e7f5e45faaeb

--- cloud/cloud/catalog/models.py
+++ cloud/cloud/catalog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    ProductName = models.CharField(max_length = 40)
+    Description = models.CharField(max_length = 40, default = 'none')
+    Price = models.DecimalField(max_digits = 10, decimal_places = 2, default = 0)
+    Quantity = models.IntegerField(default = 0)
+
+class Categories(models.Model):
+    CategoryName = models.CharField(max_length = 40)
+
+class Sales(models.Model):
+    TotalPayment = models.DecimalField(max_digits = 10, decimal_places = 2)
+    SaleDate = models.DateTimeField()
+
+class Administrators(models.Model):
+    Name = models.CharField(max_length = 40)
+    PasswordHash = models.CharField(max_length = 40)
+
+class Employees(models.Model):
+    Name = models.CharField(max_length = 40)
+    PasswordHash = models.CharField(max_length = 40)
+    IDAdmin = models.ForeignKey(Administrators, on_delete=models.CASCADE)
+
+class Products_Categories(models.Model):
+    class Meta:
+        unique_together = (('IDProduct', 'IDCategory'),)
+    IDProduct = models.ForeignKey(Products, on_delete=models.CASCADE)
+    IDCategory = models.ForeignKey(Categories, on_delete=models.CASCADE)
+
+class Sales_Products(models.Model):
+    class Meta:
+        unique_together = (('IDSale', 'IDProduct'),)
+    Quantity = models.IntegerField()
+    IDSale = models.ForeignKey(Sales, on_delete=models.CASCADE)
+    IDProduct = models.ForeignKey(Products, on_delete=models.CASCADE)
+
--- cloud/cloud/cloud/settings.py
+++ cloud/cloud/cloud/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'catalog',
+    'landing',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
