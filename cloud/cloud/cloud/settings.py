bce15274362dbc78d080752779b775f15b529e64
Barbara Valdez <babe-hime@hotmail.com>
2018-02-26 23:23:06 -0600

    Integrate landing page with django

--- cloud/cloud/cloud/settings.py
+++ cloud/cloud/cloud/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'landing',
+    'catalog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
