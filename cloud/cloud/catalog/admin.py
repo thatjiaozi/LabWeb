ebb8a3c1c25899c83d296d84b6a77a001c35f7e4
Jorge Limón Cabrera <j.limon.cabrera@gmail.com>
2018-02-27 17:15:31 -0600

    Despliegue de productos, categorias y configuracion de admin

--- cloud/cloud/catalog/admin.py
+++ cloud/cloud/catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Products
+from .models import Sales
+from .models import Administrators
+from .models import Employees
+from .models import Products_Categories
+from .models import Categories
+from .models import Sales_Products
 
 # Register your models here.
+admin.site.register(Products)
+admin.site.register(Sales)
+admin.site.register(Administrators)
+admin.site.register(Employees)
+admin.site.register(Products_Categories)
+admin.site.register(Categories)
+admin.site.register(Sales_Products)
