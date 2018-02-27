229e16c062d3b3920dc152ffedc874db7a5907b9
Jorge Limón Cabrera <j.limon.cabrera@gmail.com>
2018-02-27 15:58:32 -0600

    Template no funcional de catalogo integrado

--- cloud/cloud/catalog/views.py
+++ cloud/cloud/catalog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def catalog(request):
+    return render(request, 'catalog/catalogo.html', {})
--- cloud/cloud/cloud/urls.py
+++ cloud/cloud/cloud/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'', include('landing.urls')),
+    url(r'catalogo/', include('catalog.urls')),
 ]
