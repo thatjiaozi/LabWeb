097f4703169f9228901ae158072277843a6398e7
David de la Fuente <delafuentegarza@gmail.com>
2018-02-27 19:34:50 -0600

    Adding ids to models in Python (instead of only using autogenerated ids by Django) so we can control there are not duplicate categories

--- cloud/cloud/populate_db.py
+++ cloud/cloud/populate_db.py
@@ -1,47 +1,46 @@
+import django
 from catalog.models import Products, Categories, Sales, Administrators, Employees, Products_Categories, Sales_Products
 
 """
     Populate products
 """
 
-vasos = Products(ProductName='Paquete vasos', Description='Paquete de 5 vasos de hielo seco', Price=50, Quantity=100)
+vasos = Products(IDProduct='1', ProductName='Paquete vasos', Description='Paquete de 5 vasos de hielo seco', Price=50, Quantity=100)
 vasos.save()
 
-
-tupper = Products(ProductName='Tupper', Description='Tupper de plastico', Price=100, Quantity=30)
+tupper = Products(IDProduct='2', ProductName='Tupper', Description='Tupper de plastico', Price=100, Quantity=30)
 tupper.save()
 
-
-tina = Products(ProductName='Tina', Description='Tina infantil', Price=150, Quantity=10)
+tina = Products(IDProduct='3', ProductName='Tina', Description='Tina infantil', Price=150, Quantity=10)
 tina.save()
 
 """
     Populate Categories
 """
 
-cocina = Categories(CategoryName="Cocina")
+cocina = Categories(IDCategory='1', CategoryName="Cocina")
 cocina.save()
 
-juguetes = Categories(CategoryName="Juguetes")
+juguetes = Categories(IDCategory='2', CategoryName="Juguetes")
 juguetes.save()
 
-hogar = Categories(CategoryName="Hogar")
+hogar = Categories(IDCategory='3', CategoryName="Hogar")
 hogar.save()
 
-escolares = Categories(CategoryName="Articulos escolares")
+escolares = Categories(IDCategory='4', CategoryName="Articulos escolares")
 escolares.save()
 
 """
   Populate Sales
 """
 
-first = Sales(TotalPayment = 100, SaleDate = '2018-02-21 00:00:00')
+first = Sales(IDSale='1', TotalPayment = 100, SaleDate = '2018-02-21 00:00:00')
 first.save()
 
-second = Sales(TotalPayment = 200, SaleDate = '2018-02-22 00:00:00')
+second = Sales(IDSale='2', TotalPayment = 200, SaleDate = '2018-02-22 00:00:00')
 second.save()
 
-third = Sales(TotalPayment = 600, SaleDate = '2018-02-23 00:00:00')
+third = Sales(IDSale='3', TotalPayment = 600, SaleDate = '2018-02-23 00:00:00')
 third.save()
 
 """
@@ -57,11 +56,9 @@ tupper_hogar = Products_Categories(IDProduct = tupper, IDCategory = hogar)
 tupper_hogar.save()
 
 tina_hogar = Products_Categories(IDProduct = tina, IDCategory = hogar)
-
 tina_hogar.save()
 
 tina_escolares = Products_Categories(IDProduct = tina, IDCategory = escolares)
-
 tina_escolares.save()
 
 """
@@ -71,27 +68,23 @@ first_vasos = Sales_Products(IDSale = first, IDProduct = vasos, Quantity = 2)
 first_vasos.save()
 
 second_tupper = Sales_Products(IDSale = second, IDProduct = tupper, Quantity = 2)
-
 second_tupper.save()
 
 third_vasos = Sales_Products(IDSale = third, IDProduct = vasos, Quantity = 1)
-
 third_vasos.save()
 
 third_tupper = Sales_Products(IDSale = third, IDProduct = tupper, Quantity = 1)
-
 third_tupper.save()
 
 third_tina = Sales_Products(IDSale = third, IDProduct = tina, Quantity = 1)
-
 third_tina.save()
 
 """ 
     Populate admins
 """
 
-Jeff = Administrators(Name = "Jefferson Gutierritos", PasswordHash= "lala")
-Watts = Administrators(Name = "Watts Humphrey", PasswordHash = "PSP")
+Jeff = Administrators(IDAdmin='1', Name = "Jefferson Gutierritos", PasswordHash= "lala")
+Watts = Administrators(IDAdmin='2', Name = "Watts Humphrey", PasswordHash = "PSP")
 
 Jeff.save()
 Watts.save()
@@ -100,11 +93,8 @@ Watts.save()
     Populate Employees
 """
 
-Santana = Employees(Name = "Carlos Santana", PasswordHash="lolo", IDAdmin = Jeff)
-
+Santana = Employees(IDEmployee='1', Name = "Carlos Santana", PasswordHash="lolo", IDAdmin = Jeff)
 Santana.save()
 
-Toro = Employees(Name="Pepe el Toro", PasswordHash="123",
-        IDAdmin = Watts)
-
+Toro = Employees(IDEmployee='2', Name="Pepe el Toro", PasswordHash="123", IDAdmin = Watts)
 Toro.save()
