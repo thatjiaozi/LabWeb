007a94243625d05732c08484a08e7fea07a7306c
Juan Jose Lopez Jaimez <jj.lopezjaimez@gmail.com>
2018-03-11 17:13:57 -0600

    add model for admin app

--- local/administracion/models.py
+++ local/administracion/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Administrador(models.Model):
+    username = models.CharField(max_length = 40)
+    password_hash = models.CharField(max_length = 40)
+    full_name = models.CharField(max_length = 40)
+
+class Empleados(models.Model):
+    password_hash = models.CharField(max_length = 40)
+    username = models.CharField(max_length = 40)
+    full_name = models.CharField(max_length = 40)
+    IDAdmin = models.ForeignKey(Administrador, on_delete=models.CASCADE)
