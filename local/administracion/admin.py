de1857c150688e4acb952fec11a4c36ec5c00693
Jorge Limón Cabrera <j.limon.cabrera@gmail.com>
2018-04-03 17:52:55 -0500

    AvanceFront

--- local/administracion/admin.py
+++ local/administracion/admin.py
@@ -1,22 +1,27 @@
 from django.contrib.admin import AdminSite
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.contrib.auth import models
 
 from .models import Producto, Categoria, Venta, Folio
+
 
 class CustomAdminSite(AdminSite):
     site_header = 'Comercial Valmir'
     site_title = 'Comercial Valmir | Administracion'
     index_title = 'Administracion'
 
+
 class ProductoModelAdmin(admin.ModelAdmin):
-    list_display = ['Nombre', 'Descripcion', 'Precio', 'En_Existencia', 'Codigo']
+    list_display = ['Nombre', 'Descripcion', 'Precio', 'En_Existencia',
+                    'Codigo']
     list_editable = ['En_Existencia']
     list_filter = ['Categorias']
     search_fields = ['Nombre', 'Codigo']
 
     class Meta:
         model = Producto
+
 
 class FolioModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'Fecha']
@@ -26,6 +31,7 @@ class FolioModelAdmin(admin.ModelAdmin):
     class Meta:
         model = Folio
 
+
 class VentaModelAdmin(admin.ModelAdmin):
     list_display = ['id', '__str__', 'Cantidad']
     search_fields = ['id']
@@ -33,7 +39,9 @@ class VentaModelAdmin(admin.ModelAdmin):
     class Meta:
         model = Venta
 
+
 admin_site = CustomAdminSite(name='admin')
+
 
 class CategoriaModelAdmin(admin.ModelAdmin):
     search_fields = ['Nombre']
@@ -41,8 +49,10 @@ class CategoriaModelAdmin(admin.ModelAdmin):
     class Meta:
         model = Categoria
 
+
 class UserModelAdmin(admin.ModelAdmin):
-    list_display = ['username', 'first_name', 'last_name', 'email', 'is_staff', 'is_active']
+    list_display = ['username', 'first_name', 'last_name', 'email',
+                    'is_staff', 'is_active']
 
     def save_model(self, request, obj, form, change):
         # Override this to set the password to the value in the field if it's
@@ -54,11 +64,12 @@ class UserModelAdmin(admin.ModelAdmin):
         else:
             obj.set_password(obj.password)
         obj.save()
+
     class Meta:
         model = User
 
-admin_site = CustomAdminSite(name='admin')
 
+admin_site = CustomAdminSite(name='admin')
 admin_site.register(User, UserModelAdmin)
 admin_site.register(Producto, ProductoModelAdmin)
 admin_site.register(Categoria, CategoriaModelAdmin)
