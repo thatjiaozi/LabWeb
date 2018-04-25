dd18a8e6f24daa67818b4d4655306a3f526fea07
Juan Jose Lopez Jaimez <jj.lopezjaimez@gmail.com>
2018-04-25 16:59:17 -0500

    modify model for catalog

--- cloud/cloud/catalog/models.py
+++ cloud/cloud/catalog/models.py
@@ -24,29 +24,3 @@ class Producto(models.Model):
     class Meta:
         ordering = ('Nombre',)
 
-class Sales(models.Model):
-    TotalPayment = models.DecimalField(max_digits = 10, decimal_places = 2)
-    SaleDate = models.DateTimeField()
-
-class Administrators(models.Model):
-    Name = models.CharField(max_length = 40)
-    PasswordHash = models.CharField(max_length = 40)
-
-class Employees(models.Model):
-    Name = models.CharField(max_length = 40)
-    PasswordHash = models.CharField(max_length = 40)
-    IDAdmin = models.ForeignKey(Administrators, on_delete=models.CASCADE)
-
-class Products_Categories(models.Model):
-    class Meta:
-        unique_together = (('IDProduct', 'IDCategory'),)
-    IDProduct = models.ForeignKey(Products, on_delete=models.CASCADE)
-    IDCategory = models.ForeignKey(Categories, on_delete=models.CASCADE)
-
-class Sales_Products(models.Model):
-    class Meta:
-        unique_together = (('IDSale', 'IDProduct'),)
-    Quantity = models.IntegerField()
-    IDSale = models.ForeignKey(Sales, on_delete=models.CASCADE)
-    IDProduct = models.ForeignKey(Products, on_delete=models.CASCADE)
-
