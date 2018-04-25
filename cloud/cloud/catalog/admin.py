f027f4e3a0a737c4eef663c72c6a59f915754f58
Juan Jose Lopez Jaimez <jj.lopezjaimez@gmail.com>
2018-04-25 17:01:26 -0500

    modify model for catalog

--- cloud/cloud/catalog/admin.py
+++ cloud/cloud/catalog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Categorias
+from .models import Categoria
 from .models import Productos
 
 # Register your models here.
 admin.site.register(Productos)
-admin.site.register(Categorias)
+admin.site.register(Categoria)
