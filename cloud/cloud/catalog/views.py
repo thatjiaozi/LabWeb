d782d04cacbc789da7669e2e8032fa09f035959a
Jorge Limon Cabrera <j.limon.cabrera@gmail.com>
2018-05-09 14:30:14 -0500

    Catalogo terminado

--- cloud/cloud/catalog/views.py
+++ cloud/cloud/catalog/views.py
@@ -9,3 +9,28 @@ def catalog(request):
         'all_categories': Categoria.objects.all(),
         'all_products': Producto.objects.all()
     })
+
+def filterSideBar(request):
+    checkedCategories = request.GET.getlist('category')
+    priceFilter = request.GET['priceFilter']
+    if checkedCategories:
+        filteredProducts = Producto.objects.filter(Categorias__in=checkedCategories).filter(Precio__gt = priceFilter)
+    else:
+        filteredProducts = Producto.objects.all().filter(Precio__gt = priceFilter)
+
+    return render(request, 'catalog/catalogo.html', {
+        'all_categories': Categoria.objects.all(),
+        'all_products': filteredProducts
+    })
+
+def search(request):
+    name = request.GET['prodName']
+    if name:
+        filteredProducts = Producto.objects.all().filter(Nombre__startswith = name)
+    else:
+        filteredProducts = Producto.objects.all()
+
+    return render(request, 'catalog/catalogo.html', {
+        'all_categories': Categoria.objects.all(),
+        'all_products': filteredProducts
+    })
