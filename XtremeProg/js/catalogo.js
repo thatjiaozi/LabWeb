8c950e19bd8b8278edfa0b6e3e70d5abc56f91b8
David de la Fuente <delafuentegarza@gmail.com>
2018-05-09 16:59:53 -0500

    Merging changes of local into BackEnd_David

--- XtremeProg/js/catalogo.js
+++
@@ -1,7 +0,0 @@
-var slider = document.getElementById("priceFilter");
-var output = document.getElementById("priceLabel");
-output.innerHTML = slider.value;
-
-slider.onmouseup = function() {
-  output.innerHTML = this.value;
-}
